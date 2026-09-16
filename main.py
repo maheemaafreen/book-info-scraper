c575cedc057ef2e8d3a7dfa35a7fda37e8e59463
Maheema Afreen <afreenmaheema@gmail.com>
2026-09-16 18:37:58 +0100

    Small change

--- main.py
+++ main.py
@@ -10,26 +10,18 @@ def retrieve_webpage():
     response.raise_for_status()
     return response
 ####################
-
-response = retrieve_webpage()
-
 #SOUP PARSING
-def create_soup():
+def create_soup(response):
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
 #############
-
-soup = create_soup()
-
 #FINDING THE BOOKS
-def find_books():
+def find_books(soup):
     books = soup.find_all("article", class_="product_pod")
     return books
 #################
-books = find_books()
-
 #CREATING DATA LIST
-def get_book_data():
+def get_book_data(books):
     books_data = []
     for book in books:
         title = book.h3.a["title"]
@@ -38,16 +30,12 @@ def get_book_data():
         books_data.append(book_data)
     return books_data
 ###################
-books_data = get_book_data()
-
 #BOOK LIST
 def show_books(books_data):
     print("~~BOOKS~~")
     for number, book_data in enumerate(books_data, 1):
         print(f"{number}. {book_data["title"]} - £{book_data["price_gbp"]:.2f}")
 ##########
-show_books(books_data)
-
 #CHEAPEST BOOK
 def cheapest_book(books_data):
     cheapest_price = 1000
@@ -58,9 +46,6 @@ def cheapest_book(books_data):
             cheapest_title = book_data["title"]
     return cheapest_title, cheapest_price
 ##############
-cheapest_title, cheapest_price = cheapest_book(books_data)
-print(f"\nThe cheapest book is '{cheapest_title}', at £{cheapest_price}!\n")
-
 #MOST EXPENSIVE BOOK
 def most_expensive(books_data):
     highest_price = books_data[0]["price_gbp"]
@@ -71,9 +56,6 @@ def most_expensive(books_data):
             highest_book = book_data["title"]
     return highest_book, highest_price
 ###################
-highest_book, highest_price = most_expensive(books_data)
-print(f"The most expensive book is '{highest_book}', at £{highest_price}!")
-
 #BOOKS UNDER 20 QUID
 def under_20(books_data):
     books_less20 = []
@@ -82,12 +64,6 @@ def under_20(books_data):
             books_less20.append(book_data)
     return books_less20
 ###################
-books_less20 = under_20(books_data)
-print("\n~~Books Under 20~~")
-for number, book_less20 in enumerate(books_less20, 1):
-    print(f"{number}. {book_less20["title"]}")
-print(f"There are {len(books_less20)} books under £20.\n")
-
 #AVERAGE COST PER BOOK
 def avg_price(books_data):
     total_price = 0
@@ -96,9 +72,6 @@ def avg_price(books_data):
     average_cost = total_price / len(books_data)
     return average_cost
 ######################
-average_cost = avg_price(books_data)
-print(f"The average book price is £{average_cost:.2f}.")
-
 #CSV FILE
 def save_to_csv(books_data):
     with open("books.csv", "w", newline="") as file:
@@ -106,4 +79,21 @@ def save_to_csv(books_data):
         writer.writeheader()
         writer.writerows(books_data)
 #########
+
+response = retrieve_webpage()
+soup = create_soup(response)
+books = find_books(soup)
+books_data = get_book_data(books)
+show_books(books_data)
+cheapest_title, cheapest_price = cheapest_book(books_data)
+print(f"\nThe cheapest book is '{cheapest_title}', at £{cheapest_price}!\n")
+highest_book, highest_price = most_expensive(books_data)
+print(f"The most expensive book is '{highest_book}', at £{highest_price}!")
+books_less20 = under_20(books_data)
+print("\n~~Books Under 20~~")
+for number, book_less20 in enumerate(books_less20, 1):
+    print(f"{number}. {book_less20["title"]}")
+print(f"There are {len(books_less20)} books under £20.\n")
+average_cost = avg_price(books_data)
+print(f"The average book price is £{average_cost:.2f}.")
 save_to_csv(books_data)
